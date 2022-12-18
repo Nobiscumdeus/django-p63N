7c8b750e54e88c3924e454bae9a5b3e38d334689
Emmanuel Adeola <emmanueladeola990@gmail.com>
2022-12-18 19:38:34 +0100

    updates

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,11 +24,11 @@ load_dotenv(os.path.join(BASE_DIR,".env"))
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-rc^*w^w&6g9_(uvx#6s*bnt!w)l0rdi%!l7mv#y%uc&x%wo5pk'
+
 SECRET_KEY=os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
