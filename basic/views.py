d21a7bc2967a8091ec8dad90866e4c84c9c1ac93
Emmanuel Adeola <emmanueladeola990@gmail.com>
2022-12-18 09:20:41 +0100

    updated files

--- basic/views.py
+++ basic/views.py
@@ -4,6 +4,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('<h1> Finally, Chasfat Project$ Django applications are finding a space online after a long while </h1>')
+    return render(request,'basic/home.html')
 def html(request):
     return render(request,'basic/basic.html')
